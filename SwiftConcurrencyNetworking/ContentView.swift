bbfa3f244a676d43b191b9b86a77779462094220
Sajjad <sajjadsarkoobi@gmail.com>
2023-08-16 18:23:32 -0700

    Initial Commit

---
+++ SwiftConcurrencyNetworking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyNetworking
+//
+//  Created by Sajjad Sarkoobi on 16.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrencyNetworking/SwiftConcurrencyNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyNetworkingApp.swift
+//  SwiftConcurrencyNetworking
+//
+//  Created by Sajjad Sarkoobi on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
